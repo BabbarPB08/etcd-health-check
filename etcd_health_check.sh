87cb0b9a4a97de1f592928f19ca7b8bd875c8f16
Bharat Babbar <119395045+BabbarPB08@users.noreply.github.com>
2023-10-20 01:20:40 +0530

    Update etcd_health_check.sh

--- etcd_health_check.sh
+++ etcd_health_check.sh
@@ -1,90 +1,81 @@
 #!/bin/bash
 
-# variable
-
+# Variables
 etcd_pod=$(oc get pods -n openshift-etcd -l app=etcd --no-headers | tail -1 | awk '{print $1}')
 data_dir="/tmp/etcd_data"
 archive_name="/tmp/etcd_data.tar.gz"
 
-pre ()
-{
-mkdir -p $data_dir
+# Function to create the directory structure
+pre() {
+  mkdir -p "$data_dir"
 }
 
-post ()
-{
-rm -rf $data_dir
+# Function to remove the directory
+post() {
+  rm -rf "$data_dir"
 }
 
-pack ()
-{
-tar -czf "$archive_name" -C "$(dirname "$data_dir")" "$(basename "$data_dir")"
-if [ $? -eq 0 ]; then
-  echo "Archive created successfully: $archive_name"
-else
-  echo "Archive creation failed"
-fi
+# Function to pack data into a tar.gz archive
+pack() {
+  tar -czf "$archive_name" -C "$(dirname "$data_dir")" "$(basename "$data_dir")"
+  if [ $? -eq 0 ]; then
+    echo "Archive created successfully: $archive_name"
+  else
+    echo "Archive creation failed"
+  fi
 }
 
-etcd_table ()
-{
-oc rsh -n openshift-etcd $etcd_pod << EOF > $data_dir/etcd_table.log
-echo "---------- Member List ----------"
-etcdctl member list -w table
-echo "------------------------------------------------------------"
-echo "---------- Endpoint Health ----------"
-etcdctl endpoint health -w table
-etcdctl endpoint health --cluster
-echo "------------------------------------------------------------"
-echo "---------- Endpoint Status ----------"
-etcdctl endpoint status -w table
+# Function to display etcd member information
+etcd_table() {
+  oc rsh -n openshift-etcd "$etcd_pod" <<EOF
+    echo "---------- Member List ----------"
+    etcdctl member list -w table
+    echo "------------------------------------------------------------"
+    echo "---------- Endpoint Health ----------"
+    etcdctl endpoint health -w table
+    etcdctl endpoint health --cluster
+    echo "------------------------------------------------------------"
+    echo "---------- Endpoint Status ----------"
+    etcdctl endpoint status -w table
 EOF
 }
 
-etcd_objects_count ()
-{
-oc rsh -n openshift-etcd $etcd_pod > $data_dir/etcd_objects_count.log <<EOF
-echo "===== ETCD Keys ====="
-etcdctl get / --prefix --keys-only | grep -v ^$ | awk -F '/' '{ h[\$3]++ } END { for (k in h) print h[k], k }' | sort -nr
-echo
-echo "===== Events ====="
-etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/events | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -n
-echo
-echo "===== Secrets ====="
-etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/secrets | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -n
-echo
-echo "===== Pods ====="
-etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/pods | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -n
-EOF
+# Function to count etcd objects
+etcd_objects_count() {
+  echo -e "\n===== \nETCD Keys \n=====" 
+  oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get / --prefix --keys-only | sed '/^$/d' | cut -d/ -f3 | sort | uniq -c | sort -rn && exit 0"
+  echo -e "\n\n\n======= \nEvents \n=======" 
+  oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/events | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
+  echo -e "\n\n\n====== \nSecrets \n======" 
+  oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/secrets | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
+  echo -e "\n\n\n======= \nPods \n=======" 
+  oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/pods | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
 }
 
-metrix ()
-{
-oc exec -it -c prometheus -n openshift-monitoring prometheus-k8s-0 -- curl --data-urlencode "query=histogram_quantile(0.99, sum(rate(etcd_network_peer_round_trip_time_seconds_bucket[5m])) by (le,instance))" http://localhost:9090/api/v1/query | jq ".data.result" > $data_dir/latency.log
+# Function to display etcd network latency metrics
+metrix() {
+  oc exec -it -c prometheus -n openshift-monitoring prometheus-k8s-0 -- curl --data-urlencode "query=histogram_quantile(0.99, sum(rate(etcd_network_peer_round_trip_time_seconds_bucket[5m])) by (le,instance))" http://localhost:9090/api/v1/query | jq ".data.result"
 }
 
-obj_size ()
-{
-oc rsh -n openshift-etcd $etcd_pod > $data_dir/etcd_object_size.log <<EOF
-for base in kubernetes openshift; do
-    readarray -t RESOURCES <<< \
-        "\$(etcdctl --command-timeout=30s \
-        get --prefix / --keys-only 2>/dev/null | grep -oP "(?<=/\${base}.io/).+?(?=/)" | sort | uniq)"
-    for res in "\${RESOURCES[@]}"; do
-        BYTES=\$(etcdctl --command-timeout=30s \
-            get --prefix /\${base}.io/\$res -w protobuf | wc -c)
-        COUNT=\$(etcdctl --command-timeout=30s \
-            get --prefix /\${base}.io/\$res --keys-only | sed '/^$/d' | wc -l)
+# Function to calculate the size of etcd objects
+obj_size() {
+  oc rsh -n openshift-etcd "$etcd_pod" <<EOF
+    for base in kubernetes openshift; do
+      readarray -t RESOURCES <<< "$(etcdctl --command-timeout=30s get --prefix / --keys-only 2>/dev/null | grep -oP "(?<=/\${base}.io/).+?(?=/)" | sort | uniq)"
+      for res in "\${RESOURCES[@]}"; do
+        BYTES=\$(etcdctl --command-timeout=30s get --prefix /\${base}.io/\$res -w protobuf | wc -c)
+        COUNT=\$(etcdctl --command-timeout=30s get --prefix /\${base}.io/\$res --keys-only | sed '/^$/d' | wc -l)
         echo -e "Count=\$COUNT  \tBytes=\$BYTES\tObject=/\${base}.io/\$res"
+      done
     done
-done
 EOF
 }
 
+# Main script (function calling)
 pre
-etcd_table
-etcd_objects_count
-obj_size
-metrix
+etcd_table > "$data_dir/etcd_table.log"
+etcd_objects_count &> "$data_dir/etcd_objects_count.log"
+obj_size &> "$data_dir/etcd_objects_size.log"
+metrix &> "$data_dir/latency.log"
 pack
 post
