ac1f5d52e75caa7ff1e39b49e8ac37a07a8571de
Bharat Babbar <119395045+BabbarPB08@users.noreply.github.com>
2025-01-25 02:06:31 +0530

    Update etcd_health_check.sh

--- etcd_health_check.sh
+++ etcd_health_check.sh
@@ -42,13 +42,13 @@ EOF
 
 # Function to count etcd objects
 etcd_objects_count() {
-  echo -e "\n===== \nETCD Keys \n=====" 
+  echo -e "\n===== \nETCD Keys \n====="
   oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get / --prefix --keys-only | sed '/^$/d' | cut -d/ -f3 | sort | uniq -c | sort -rn && exit 0"
-  echo -e "\n\n\n======= \nEvents \n=======" 
+  echo -e "\n\n\n======= \nEvents \n======="
   oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/events | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
-  echo -e "\n\n\n====== \nSecrets \n======" 
+  echo -e "\n\n\n====== \nSecrets \n======"
   oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/secrets | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
-  echo -e "\n\n\n======= \nPods \n=======" 
+  echo -e "\n\n\n======= \nPods \n======="
   oc rsh -n openshift-etcd "$etcd_pod" bash -c "etcdctl --command-timeout=60s get --prefix --keys-only /kubernetes.io/pods | awk -F/ '/./ { print \$4 }' | sort | uniq -c | sort -rn && exit 0"
 }
 
@@ -57,25 +57,22 @@ metrix() {
   oc exec -it -c prometheus -n openshift-monitoring prometheus-k8s-0 -- curl --data-urlencode "query=histogram_quantile(0.99, sum(rate(etcd_network_peer_round_trip_time_seconds_bucket[5m])) by (le,instance))" http://localhost:9090/api/v1/query | jq ".data.result"
 }
 
-# Function to calculate the size of etcd objects
-obj_size() {
-  oc rsh -n openshift-etcd "$etcd_pod" <<EOF
-    for base in kubernetes openshift; do
-      readarray -t RESOURCES <<< "$(etcdctl --command-timeout=30s get --prefix / --keys-only 2>/dev/null | grep -oP "(?<=/\${base}.io/).+?(?=/)" | sort | uniq)"
-      for res in "\${RESOURCES[@]}"; do
-        BYTES=\$(etcdctl --command-timeout=30s get --prefix /\${base}.io/\$res -w protobuf | wc -c)
-        COUNT=\$(etcdctl --command-timeout=30s get --prefix /\${base}.io/\$res --keys-only | sed '/^$/d' | wc -l)
-        echo -e "Count=\$COUNT  \tBytes=\$BYTES\tObject=/\${base}.io/\$res"
-      done
-    done
-EOF
+# Function to calculate the size of etcd objects, sorted by count
+analyze_object_sizes() {
+  echo -e "\n===== Analyzing ETCD Object Sizes ====="
+  oc exec -n openshift-etcd -c etcdctl "$etcd_pod" -- sh -c "etcdctl get / --prefix --keys-only | grep -oE '^/[a-z|.]+/[a-z|.|8]*' | sort | uniq -c | sort -rn" | \
+  while read -r KEY; do
+    printf "$KEY\t"
+    oc exec -n openshift-etcd -c etcdctl "$etcd_pod" -- etcdctl get "${KEY##* }" --prefix --write-out=json | \
+    jq '[.kvs[].value | length] | add' | numfmt --to=iec
+  done | sort -k1,1 -nr | column -t
 }
 
 # Main script (function calling)
 pre
 etcd_table > "$data_dir/etcd_table.log"
 etcd_objects_count &> "$data_dir/etcd_objects_count.log"
-obj_size &> "$data_dir/etcd_objects_size.log"
+analyze_object_sizes &> "$data_dir/etcd_objects_size.log"
 metrix &> "$data_dir/latency.log"
 pack
 post
